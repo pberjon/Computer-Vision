3a1517ea2f58587495d590232f9c7178bced6c48
pberjon <57068021+pberjon@users.noreply.github.com>
2023-03-22 10:33:31 +0100

    Create dota_to_coco.py
    Commit the DOTAtoCOCO convertor. The script takes two arguments (--dota-path and --coco-path) and you need to have a DOTA dataset with this kind of architecture:
    -DOTADataset
       - train
          - images
             - *.png
          - labelTxt
             - *.txt
       - val
          - images
             - *.png
          - labelTxt
             - *.txt

---
+++ DatatypesConvertors/dota_to_coco.py
@@ -0,0 +1,171 @@
+import json
+import os
+import shutil
+import datetime
+from shapely import Polygon
+
+e = datetime.datetime.now()
+
+CLASSES = ['wind_turbine']
+IMG_HEIGHT = 512
+IMG_WIDTH = 512
+
+year = "2023"
+version = "1.0"
+description = "COCO annotation file for Wind Turbines Dataset"
+contributor = "Pierre Berjon"
+url = "github.com"
+date_created = "%s-%s-%sT%s:%s:%s" % (e.year, e.month, e.day, e.hour, e.minute, e.second)
+
+def get_categoryID(categoriesList, category):
+    for cat in categoriesList:
+        if cat["name"] == category:
+            return cat["id"]
+    return None
+
+def get_imageID(imagesList, filename):
+    for image in imagesList:
+        if image["file_name"] == filename:
+            return image["id"]
+    return None
+
+class DOTAtoCOCO():
+    def __init__(self, path_to_DOTA, path_to_COCO, labels, height, width):
+        self.COCOpath = path_to_COCO
+        self.DOTApath = path_to_DOTA
+
+        self.dotaLabels = labels
+        self.imagesHeight = height
+        self.imagesWidth = width
+        
+    
+    def get_info(self):
+        return {
+            "year": year,
+            "version": version,
+            "description": description,
+            "contributor": contributor,
+            "url": url,
+            "date_created": date_created
+        }
+    
+    def get_categories(self):
+        labels = self.dotaLabels
+        categories = []
+        for i in range(len(labels)):
+            label = labels[i]
+            categories.append({
+                "id": i,
+                "name": label
+            })
+        
+        return categories
+    
+    def get_images(self):
+        images = []
+        imagesFiles = os.listdir(os.path.join(self.DOTApath, 'images'))
+        for idx in range(len(imagesFiles)):
+            imageFile = imagesFiles[idx]
+            image = {
+                "id": idx,
+                "license": 1,
+                "file_name": imageFile,
+                "height": self.imagesHeight,
+                "width": self.imagesWidth,
+                "date_captured": None
+            }
+            images.append(image)
+        
+        return images
+
+    def get_annotations(self):
+        annotations = []
+        labelsFiles = os.listdir(os.path.join(self.DOTApath, 'labelTxt'))
+        images = self.get_images()
+        categories = self.get_categories()
+        record_id = 0
+        for idx in range(len(labelsFiles)):
+            labelFile = labelsFiles[idx]
+            labelName, labelExtension = os.path.splitext(labelFile)
+            imageID = get_imageID(images, labelName + '.png')
+            with open(os.path.join(self.DOTApath, 'labelTxt', labelFile)) as f:
+                lines = f.readlines()
+            for i in range(len(lines)):
+                line = lines[i]
+                line = line.split(' ')
+                coords = line[:-3]
+                bbox = [float(coords[0]), float(coords[1]), float(coords[4]), float(coords[5])]
+                poly = Polygon([[coords[i], coords[i+1]] for i in [0,2,4,6]])
+                area = poly.area
+                category = line[-3]
+                label = {
+                    "id": record_id,
+                    "image_id": imageID,
+                    "category_id": get_categoryID(categories, category),
+                    "bbox": bbox,
+                    "area": area
+                }
+
+                record_id += 1
+
+                annotations.append(label)
+        return annotations
+
+    def copy_images(self):
+        for imageFile in os.listdir(os.path.join(self.DOTApath, 'images')):
+            shutil.copy(
+                os.path.join(self.DOTApath, 'images', imageFile),
+                os.path.join(self.COCOpath, imageFile)
+            )
+
+    def process(self):
+        info = self.get_info()
+        categories = self.get_categories()
+        images = self.get_images()
+        annotations = self.get_annotations()
+        coco_data = CocoAnnotation(info, categories, images, annotations)
+
+        coco_data.save_annotation(os.path.join(self.COCOpath, 'annotation.json'))
+        # self.copy_images()
+
+class CocoAnnotation():
+    def __init__(self, info, categories, images, annotations):
+        self.info = info
+        self.categories = categories
+        self.images = images
+        self.annotations = annotations
+    
+    def get_annotation(self):
+        ann = {}
+        ann["info"] = self.info
+        ann["categories"] = self.categories
+        ann["images"] = self.images
+        ann["annotations"] = self.annotations
+
+        return ann
+    
+    def save_annotation(self, destination_path):
+        ann = self.get_annotation()
+        with open(destination_path, "w") as fp:
+            json.dump(ann, fp)
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert DOTA dataset to COCO dataset')
+    parser.add_argument('--dota-path', help='Path to DOTA dataset')
+    parser.add_argument('--coco-path', help='Path to COCO dataset')
+    args = parser.parse_args()
+
+    return args
+
+args = parse_args()
+
+# os.makedirs(os.path.join(args.coco_path, 'train'), exist_ok=True)
+# os.makedirs(os.path.join(args.coco_path, 'val'), exist_ok=True)
+print('Converting train dataset...')
+TrainConvertor = DOTAtoCOCO(os.path.join(args.dota_path, 'train'), os.path.join(args.coco_path, 'train'), CLASSES, IMG_HEIGHT, IMG_WIDTH)
+TrainConvertor.process()
+print('Converting val dataset...')
+ValConvertor = DOTAtoCOCO(os.path.join(args.dota_path, 'val'), os.path.join(args.coco_path, 'val'), CLASSES, IMG_HEIGHT, IMG_WIDTH)
+ValConvertor.process()
